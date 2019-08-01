b7b0270584db1f0281e8abd9ae60d13e80b249e1
Alfred V Jose <alfredzeee@gmail.com>
2019-08-01 18:42:39 +0530

    Adding Contour Detection.

--- Image Processing/main.py
+++ Image Processing/main.py
@@ -17,7 +17,57 @@ def shape_selection(event, x, y, flags, param):
 		print (ref_point)
 		print (image[ref_point[0][1], ref_point[0][0]])
 
-		
+		hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+		d= hsv.item(y,x,0)
+		e= hsv.item(y,x,1)
+		f= hsv.item(y,x,2)
+		print(d, e, f)
+		if (d+20 >= 360):
+			dup = 360
+		else:
+			dup = d+20
+
+		if (d-20 <= 0):
+			dlow = 0
+		else:
+			dlow = d-20
+
+		if (e+20 >= 255):
+			eup = 255
+		else:
+			eup = e+20
+
+		if (e-20 <= 0):
+			elow = 0
+		else:
+			elow = e-20
+
+		if (f+20 >= 255):
+			fup = 255
+		else:
+			fup = f+20
+
+		if (f-20 <= 0):
+			flow = 0
+		else:
+			flow = f-20
+
+		print(dup, dlow, eup, elow, fup, flow)
+		lower_color = np.array([dlow,elow,flow])
+		upper_color = np.array([dup,255,255])
+
+		mask = cv2.inRange(hsv, lower_color, upper_color)
+		# cv2.imshow('Mask', mask)
+
+		contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+		for contour in contours:
+			if (cv2.pointPolygonTest(contour,ref_point[0], False)) == 1:
+				print("point is inside contour")
+				cv2.drawContours(image, contour, -1, (0, 255, 0), 3)
+
+		cv2.imshow("Image", image)
+
      
 cv2.setMouseCallback("Image", shape_selection)
 
